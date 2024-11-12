dbb83a8e9370cc8bd15b8d6d08969b5fd3d0e44c
Enkhsolongo10 <rrainbow1018@gmail.com>
2024-11-12 18:11:03 +0800

    classes

--- todo.js
+++ todo.js
@@ -1,35 +1,5 @@
-const STATUS = "TODO" | "DONE";
-const todos = [
-    {
-        name: "clean my home",
-        status: "TODO"
-    },
-];
-console.log("init todo", todos);
-//todo add 
-function addOne(newTodo) {
-    todos.push(newTodo);
-}    
-
-//status uurchluh 
-function editStatus(index, status) {
-    let item = todos[index];
-    item.status = status;
+function addTodo() {
+    const input = prompt("Enter todo name");
+    addOne({name:input, status: "TODO"});
+    render();
 }
-
-//ner uurchluh: func
-function editName() {}
-
-//todo delete one item
-function deleteOne() {}
-
-addOne({name:"hool hiih", status:"TODO",});
-addOne({name:"javascript sudlah", status:"TODO",});
-console.log("Add----", todos);
-
-//RUNNIG APPLICATION 
-// addOne({name:"hool hiih", status:"TODO"});
-// addOne({name:"JS sudlah", status:"TODO"});
-// addOne({name:"hool hiih", status:"TODO"});
-// editStatus(1, "DONE"); 
-
